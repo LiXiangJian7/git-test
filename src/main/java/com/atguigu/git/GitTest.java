d07f6e8f1fbfe48620c0244333335b56f5a311aa
Yolo <1720820714@qq.com>
2021-09-17 10:42:48 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author LXJ-Start
+ * 2021-09-17-10:34
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
